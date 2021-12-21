851708a272b15b8fe132b86e8bcbc8e83827e4fc
Bob Reselman <bob@CogArtTech.com>
2021-12-21 13:57:39 -0800

    Update biz-app-administrator-cert.sh

--- rbac/biz-app-administrator-cert.sh
+++ rbac/biz-app-administrator-cert.sh
@@ -20,7 +20,7 @@ kind: CertificateSigningRequest
 metadata:
   name: $CERTIFICATE_NAME 
 spec:
-  signerName: $USERNAME
+  signerName: example.com/$USERNAME
   groups:
   - system:authenticated
   request: $(cat $CSR_FILE | base64 | tr -d '\n')
